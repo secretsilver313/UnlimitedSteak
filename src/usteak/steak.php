8e96b6328dbd221f4b9e70a9b5e4360edfce9b7a
secretsilver313 <64828266+secretsilver313@users.noreply.github.com>
2020-06-03 14:34:59 -0400

    Update steak.php

--- src/usteak/steak.php
+++ src/usteak/steak.php
@@ -1,15 +1,48 @@
 <?php
 
+namespace usteak;
+
+use pocketmine\Player;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerConsumeEvent;
+
+
+
 public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
  {
  if($cmd->getName() == "unlimsteak") {
- if($sender->hasPermission("unlimsteak.use")) {
+  if($sender instanceof Player) {
+   if($sender->hasPermission("unlimsteak.use")) {
+   $inv = $sender->getinventory;
+   $steak = Item::get(364, 0, 1);
+   $steak->setCustomName("Unlimited Steak");
+   $steak->setLore(["Infinate Steak"]);
+    $sender->$inv->addItem($steak);
+
+   }
+  }else{
+  $player->sendMessage("§4You cant use this command!");
+  }
+  return true;
+ }
+}
+
+
+public Function onConsume(PlayerConsumeEvent $event, Player $player)
+{
+         $item = $event->getItem();
+         $player = $event->getPlayer();
+         $steak = Item::get(364, 0, 1);
+         $inv = $player->getinventory;
+         $hunger = $player->getHunger
+ $steak->setCustomName("Unlimited Steak");
+ $steak->setLore(["Infinate Steak"]);
  
-$inv = $player->getinventory;
-$steak = Item::get(364, 0, 1);
-$steak->setCustomName("Unlimited Steak")
+ if($item->getLore() == $steak->getLore(){
+  $player->$inv->addItem($steak);
+  if($hunger == getMaxHunger){
+   $player->sendMessage("§2You have fully replenished your hunger");
+  }
+ }
+}
 
-
-
-
-<?
